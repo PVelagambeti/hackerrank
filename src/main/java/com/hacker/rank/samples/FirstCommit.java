3c3baa69c1e907395258f9d1358c671751518a06
Praveen <praveen.velagambeti@bhnetwork.com>
2018-07-25 18:59:59 -0700

    Few more changes

--- src/main/java/com/hacker/rank/samples/FirstCommit.java
+++ src/main/java/com/hacker/rank/samples/FirstCommit.java
@@ -3,7 +3,7 @@ package com.hacker.rank.samples;
 public class TestProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello..!!");
+        System.out.println("Hello, my first commit to this repo..!!");
     }
 
 }
